5d6ccf78a912539f05bd9510e05ce3dfa699322e
OmarFarukMaruf <mdomarfaruk.cse@gmail.com>
2026-03-03 00:42:16 +0600

    addToWatchList Bugs fixes

---
+++ prisma/migrations/20260302183544_add_title_to_watchlist/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "WachingListItem" ADD COLUMN     "title" TEXT;
--- src/controller/authController.js
+++ src/controller/authController.js
@@ -29,12 +29,12 @@ const register = async (req, res) => {
         }
     });
 
-    const token = generateToken(user.id, res);
+    const token = generateToken(newUser.id, res);
 
     res.status(201).json({
         status: "success", data: {
             user: {
-                id: user.id,
+                id: newUser.id,
                 name: name,
                 email: email
             },
@@ -85,10 +85,12 @@ const logout = async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
-        expires: new Date(0),});
-    res.status(200).json({ 
+        expires: new Date(0),
+    });
+    res.status(200).json({
         status: "success",
-        message: "Logged out successfully" });
+        message: "Logged out successfully"
+    });
 };
 
 export { register, login, logout };
